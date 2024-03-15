97acf24d676d4246bb29f8c9ac12333fc6fe70e1
MphoCM <christian.mofokeng@gmail.com>
2024-03-15 22:16:37 +0000

    update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,20 @@
-class Building {
+export default class Building {
   constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
     this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
-  // Getter for sqft attribute
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method that must be overridden by subclasses
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
-
-export default Building;
