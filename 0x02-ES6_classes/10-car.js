b436d0ab21194fb66e8b643ded063e5949a93deb
MphoCM <christian.mofokeng@gmail.com>
2024-03-15 22:24:24 +0000

    update

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,21 +1,11 @@
-// Define a symbol for the cloneCar method
-const cloneCarSymbol = Symbol('cloneCar');
-
-// Define the Car class
 export default class Car {
-  // Constructor to initialize brand, motor, and color attributes
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  // Method to clone the car
-  [cloneCarSymbol]() {
+  cloneCar() {
     return new this.constructor(this._brand, this._motor, this._color);
   }
-
-  // Method to call the cloneCar method
-  cloneCar() {
-    return this[cloneCarSymbol]();
-  }
+}
